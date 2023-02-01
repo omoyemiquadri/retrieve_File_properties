846bd84b99c8c31258267e692712e5c75c8921e9
Omoyemi Quadri <56293002+omoyemiquadri@users.noreply.github.com>
2023-02-01 18:29:18 +0100

    Add files via upload

---
+++ file_properties.py
@@ -0,0 +1,13 @@
+print('A python program to retrieve file properties')
+print("")
+print("")
+import os.path
+import time
+
+print('File         :', __file__)
+print('Access time  :', time.ctime(os.path.getatime(__file__)))
+print('Modified time:', time.ctime(os.path.getmtime(__file__)))
+print('Change time  :', time.ctime(os.path.getctime(__file__)))
+print('Size         :', os.path.getsize(__file__))
+			 
+	
